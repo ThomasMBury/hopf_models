0a2bc0ec0f695032ec216a8ad5a9fc84743ca331
Thomas Bury <tombury182@gmail.com>
2018-12-07 20:42:44 -0500

    grid plot of power spectrum evolution

--- cr_model/cr_ews.py
+++ cr_model/cr_ews.py
@@ -13,14 +13,13 @@ Code to simulate the RM model and compute EWS
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
+import seaborn as sns
 import os
 
 # import EWS function
 import sys
 sys.path.append('../../early_warnings')
 from ews_compute import ews_compute
-from ews_spec import pspec_welch, pspec_metrics
 
 
 #---------------------
@@ -39,14 +38,13 @@ if not os.path.exists('data_export/'+dir_name):
 #–-----------------------------
 
 
-
 # Simulation parameters
 dt = 0.1
 t0 = 0
 tmax = 150
 tburn = 100 # burn-in period
-numSims = 100
-seed = 21 # random number generation seed
+numSims = 5
+seed = 0 # random number generation seed
 
 # EWS parameters
 rw = 0.25 # rolling window
@@ -55,7 +53,7 @@ lags = [1,2,4] # autocorrelation lag times
 ews = ['var','ac','sd','cv','skew','kurt','smax','aic'] # EWS to compute
 ham_length = 40 # number of data points in Hamming window
 ham_offset = 0.5 # proportion of Hamming window to offset by upon each iteration
-pspec_roll_offset = 5 # offset for rolling window when doing spectrum metrics
+pspec_roll_offset = 20 # offset for rolling window when doing spectrum metrics
 
 
 #----------------------------------
@@ -146,7 +144,7 @@ for j in range(numSims):
     df_sims_x['Sim '+str(j+1)] = series_x
     df_sims_y['Sim '+str(j+1)] = series_y
 
-
+print('\nSimulations complete\n')
 
 
 #----------------------
@@ -159,10 +157,11 @@ df_sims_filt = df_sims_x[np.remainder(df_sims_x.index,dt2) == 0]
 
 # set up a list to store output dataframes from ews_compute- we will concatenate them at the end
 appended_ews = []
+appended_pspec = []
 
 # loop through each trajectory as an input to ews_compute
 for i in range(numSims):
-    df_temp = ews_compute(df_sims_filt['Sim '+str(i+1)], 
+    ews_dic = ews_compute(df_sims_filt['Sim '+str(i+1)], 
                       roll_window = rw, 
                       band_width = bw,
                       lag_times = lags, 
@@ -171,22 +170,32 @@ for i in range(numSims):
                       ham_offset = ham_offset,
                       pspec_roll_offset = pspec_roll_offset,
                       upto=tbif)
-    # include a column in the dataframe for realisation number
-    df_temp['Realisation number'] = pd.Series((i+1)*np.ones([len(t)],dtype=int),index=t)
+    # The DataFrame of EWS
+    df_ews_temp = ews_dic['EWS metrics']
+    # The DataFrame of power spectra
+    df_pspec_temp = ews_dic['Power spectrum']
     
-    # add DataFrame to list
-    appended_ews.append(df_temp)
+    # Include a column in the DataFrames for realisation number
+    df_ews_temp['Realisation number'] = (i+1)*np.ones([len(df_ews_temp)],dtype=int)
+    df_pspec_temp['Realisation number'] = (i+1)*np.ones([len(df_pspec_temp)],dtype=int)
     
-    # print status every 10 realisations
+    
+    # Add DataFrames to list
+    appended_ews.append(df_ews_temp)
+    appended_pspec.append(df_pspec_temp)
+    
+    # Print status every realisation
     if np.remainder(i+1,1)==0:
-        print('Realisation '+str(i+1)+' complete')
+        print('EWS for realisation '+str(i+1)+' complete')
 
 
-# concatenate EWS DataFrames - use realisation number and time as indices
+# Concatenate EWS DataFrames - use realisation number and time (and frequency for df_pspec) as indices
 df_ews = pd.concat(appended_ews).set_index('Realisation number',append=True).reorder_levels([1,0])
+df_pspec = pd.concat(appended_pspec).set_index('Realisation number',append=True).reorder_levels([2,0,1])
 
-# export the first 5 realisations for plotting
-df_ews.loc[1:5].to_csv('data_export/'+dir_name+'/ews_singles.csv')
+
+## export the first 5 realisations for plotting
+#df_ews.loc[1:5].to_csv('data_export/'+dir_name+'/ews_singles.csv')
 
 
 # Compute summary statistics of EWS over all realisations (mean, pm1 s.d.)
@@ -194,80 +203,120 @@ df_ews_means = df_ews[['Variance', 'Lag-1 AC', 'Lag-2 AC', 'Lag-4 AC',
                       'AIC fold', 'AIC hopf']].mean(level='Time')
 df_ews_deviations = df_ews[['Variance', 'Lag-1 AC', 'Lag-2 AC', 'Lag-4 AC', 
                       'AIC fold', 'AIC hopf']].std(level='Time')
-# Export summary statistics for plotting in MMA
-df_ews_means.to_csv('data_export/'+dir_name+'/ews_mean.csv')
-df_ews_deviations.to_csv('data_export/'+dir_name+'/ews_std.csv')
 
 
-#--------------------
+## Export summary statistics for plotting in MMA
+#df_ews_means.to_csv('data_export/'+dir_name+'/ews_mean.csv')
+#df_ews_deviations.to_csv('data_export/'+dir_name+'/ews_std.csv')
+
+
+#-------------------------
 # Plots to visualise EWS
 #-------------------------
 
+# Realisation number to plot
+plot_num = 2
+
 ## Plot of trajectory, smoothing and EWS
-plot_num = 1
 fig1, axes = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(6,6))
-df_ews.loc[plot_num][['State variable','Smoothing']].plot(ax=axes[0],title='Early warning signals for the Consumer Resource Model')
+df_ews.loc[plot_num][['State variable','Smoothing']].plot(ax=axes[0],
+          title='Early warning signals for a single realisation')
 df_ews.loc[plot_num]['Variance'].plot(ax=axes[1],legend=True)
 df_ews.loc[plot_num][['Lag-1 AC','Lag-2 AC','Lag-4 AC']].plot(ax=axes[1], secondary_y=True,legend=True)
 df_ews.loc[plot_num]['Smax'].dropna().plot(ax=axes[2],legend=True)
 df_ews.loc[plot_num][['AIC fold','AIC hopf','AIC null']].dropna().plot(ax=axes[3],legend=True)
 
 
-## Power spectrum and fitting at t = t_pspec
+## Grid plot for evolution of the power spectrum in time
+g = sns.FacetGrid(df_pspec.loc[plot_num].reset_index(), 
+                  col='Time',
+                  col_wrap=3,
+                  sharey=False,
+                  aspect=1.5,
+                  size=1.8
+                  )
 
-# Chosen time to visualise power spectrum
-t_pspec = 140
-
-# Use pspec_welch to compute power spectrum
-pspec=pspec_welch(df_ews.loc[plot_num][t_pspec-rw*max(df_sims_filt.index):t_pspec]['Residuals'], dt2, ham_length=ham_length, w_cutoff=1)
-
-# Execute the function pspec_metrics to compute the AIC weights and fitting parameters
-spec_ews = pspec_metrics(pspec, ews=['smax', 'cf', 'aic', 'aic_params'])
-# Define the power spectrum models
-def fit_fold(w,sigma,lam):
-    return (sigma**2 / (2*np.pi))*(1/(w**2+lam**2))
-        
-def fit_hopf(w,sigma,mu,w0):
-    return (sigma**2/(4*np.pi))*(1/((w+w0)**2+mu**2)+1/((w-w0)**2 +mu**2))
-        
-def fit_null(w,sigma):
-    return sigma**2/(2*np.pi)* w**0
-
-# Make plot
-w_vals = np.linspace(-max(pspec.index), max(pspec.index), 100)
-fig2 = plt.figure(2)
-pspec.plot(label='Measured')
-plt.plot(w_vals, fit_fold(w_vals, spec_ews['Params fold']['sigma'], spec_ews['Params fold']['lam']),label='Fold (AIC='+str(round(spec_ews['AIC fold'],2))+')')
-plt.plot(w_vals, fit_hopf(w_vals, spec_ews['Params hopf']['sigma'], spec_ews['Params hopf']['mu'], spec_ews['Params hopf']['w0']),label='Hopf (AIC='+str(round(spec_ews['AIC hopf'],2))+')')
-plt.plot(w_vals, fit_null(w_vals, spec_ews['Params null']['sigma']),label='Null (AIC='+str(round(spec_ews['AIC null'],2))+')')
-plt.ylabel('Power')
-plt.legend()
-plt.title('Power spectrum and fits at time t='+str(t_pspec))
+g.map(plt.plot, 'Frequency', 'Empirical', color='k', linewidth=2)
+g.map(plt.plot, 'Frequency', 'Fit fold', color='b', linestyle='dashed', linewidth=1)
+g.map(plt.plot, 'Frequency', 'Fit hopf', color='r', linestyle='dashed', linewidth=1)
+g.map(plt.plot, 'Frequency', 'Fit null', color='g', linestyle='dashed', linewidth=1)
+# Axes properties
+axes = g.axes
+# Set y labels
+for ax in axes[::3]:
+    ax.set_ylabel('Power')
+## Set y limit as max power over all time
+#for ax in axes:
+#    ax.set_ylim(top=1.05*max(df_pspec['Empirical']), bottom=0)
 
 
-## Plot of power spectrum evolution over time
 
-# initialise dataframe for power spectra
-df_pspec = pd.DataFrame([])
-# loop over time
-for k in np.arange(rw*tbif, tbif, 10):
-            
-            # select subset of series contained in window
-            window_series = df_ews.loc[1]['Residuals'][k-rw*tbif:k]
-                        
-            # compute power spectrum of window data using function pspec_welch
-            pspec = pspec_welch(window_series, dt2, 
-                                ham_length=ham_length, 
-                                ham_offset=ham_offset)
-            # add series to dataframe
-            df_pspec[str(k)] = pspec
-            
-# plot evolving power spectrum
-fig4 = plt.figure(4)            
-df_pspec.plot()
 
-# export power spectra data for plotting in MMA
-df_pspec.to_csv('data_export/'+dir_name+'/pspec.csv')
+
+
+
+
+
+
+
+
+
+
+### Power spectrum and fitting at t = t_pspec
+#
+## Chosen time to visualise power spectrum
+#t_pspec = 140
+#
+## Use pspec_welch to compute power spectrum
+#pspec=pspec_welch(df_ews.loc[plot_num][t_pspec-rw*max(df_sims_filt.index):t_pspec]['Residuals'], dt2, ham_length=ham_length, w_cutoff=1)
+#
+## Execute the function pspec_metrics to compute the AIC weights and fitting parameters
+#spec_ews = pspec_metrics(pspec, ews=['smax', 'cf', 'aic', 'aic_params'])
+## Define the power spectrum models
+#def fit_fold(w,sigma,lam):
+#    return (sigma**2 / (2*np.pi))*(1/(w**2+lam**2))
+#        
+#def fit_hopf(w,sigma,mu,w0):
+#    return (sigma**2/(4*np.pi))*(1/((w+w0)**2+mu**2)+1/((w-w0)**2 +mu**2))
+#        
+#def fit_null(w,sigma):
+#    return sigma**2/(2*np.pi)* w**0
+#
+## Make plot
+#w_vals = np.linspace(-max(pspec.index), max(pspec.index), 100)
+#fig2 = plt.figure(2)
+#pspec.plot(label='Measured')
+#plt.plot(w_vals, fit_fold(w_vals, spec_ews['Params fold']['sigma'], spec_ews['Params fold']['lam']),label='Fold (AIC='+str(round(spec_ews['AIC fold'],2))+')')
+#plt.plot(w_vals, fit_hopf(w_vals, spec_ews['Params hopf']['sigma'], spec_ews['Params hopf']['mu'], spec_ews['Params hopf']['w0']),label='Hopf (AIC='+str(round(spec_ews['AIC hopf'],2))+')')
+#plt.plot(w_vals, fit_null(w_vals, spec_ews['Params null']['sigma']),label='Null (AIC='+str(round(spec_ews['AIC null'],2))+')')
+#plt.ylabel('Power')
+#plt.legend()
+#plt.title('Power spectrum and fits at time t='+str(t_pspec))
+#
+#
+### Plot of power spectrum evolution over time
+#
+## initialise dataframe for power spectra
+#df_pspec = pd.DataFrame([])
+## loop over time
+#for k in np.arange(rw*tbif, tbif, 10):
+#            
+#            # select subset of series contained in window
+#            window_series = df_ews.loc[1]['Residuals'][k-rw*tbif:k]
+#                        
+#            # compute power spectrum of window data using function pspec_welch
+#            pspec = pspec_welch(window_series, dt2, 
+#                                ham_length=ham_length, 
+#                                ham_offset=ham_offset)
+#            # add series to dataframe
+#            df_pspec[str(k)] = pspec
+#            
+## plot evolving power spectrum
+#fig4 = plt.figure(4)            
+#df_pspec.plot()
+#
+## export power spectra data for plotting in MMA
+#df_pspec.to_csv('data_export/'+dir_name+'/pspec.csv')
 
 
 
